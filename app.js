7fd4d29c0e2ed79cc5b5537a50c389f6d82dda9f
deniskodak <denis.kodak@gmail.com>
2021-10-02 22:49:45 +0300

    Создает архитектуру папок проекта, конектит к базе данных и запускает сервер

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("It works!");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((_, res) => {
+  res.status(404).json({ status: "error", code: 404, message: "Not found" });
+});
+
+app.use((error, _, res, __) => {
+  const { status = 500, message = "Server error" } = error;
+  res.status(status).json({
+    status: "error",
+    code: status,
+    message,
+  });
+});
+module.exports = app;
