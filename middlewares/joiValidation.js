a68a5169878782ec4a5620331add627152cafe84
deniskodak <denis.kodak@gmail.com>
2021-10-03 23:36:30 +0300

    Добавляет прослойку joiValidation

---
+++ middlewares/joiValidation.js
@@ -0,0 +1,15 @@
+const joiValidation = (schema) => {
+  validFunc = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+    next();
+  };
+
+  return validFunc;
+};
+
+module.exports = joiValidation;
