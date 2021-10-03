635ec0084476a32340867c0901c54e75a868abe2
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-10-03 22:17:46 +0300

    Пофиксил имя переменной

--- routes/api/transactionsRouter.js
+++ routes/api/transactionsRouter.js
@@ -5,12 +5,12 @@ const { controllerWrapper } = require("../../middlewares")
 
 // router.get('/', ctrl.getAllBanks);
 
-transactionRouter.patch('/incomes', controllerWrapper(ctrl.addIncome));
+transactionsRouter.patch('/incomes', controllerWrapper(ctrl.addIncome));
 
-transactionRouter.patch('/expenses', controllerWrapper(ctrl.addExpense));
+transactionsRouter.patch('/expenses', controllerWrapper(ctrl.addExpense));
 
-transactionRouter.patch('/balance', controllerWrapper(ctrl.renewBalance));
+transactionsRouter.patch('/balance', controllerWrapper(ctrl.renewBalance));
 
-transactionRouter.delete('/:operationType/:operationId', controllerWrapper(ctrl.deleteTransaction));
+transactionsRouter.delete('/:operationType/:operationId', controllerWrapper(ctrl.deleteTransaction));
 
 module.exports = transactionsRouter;
