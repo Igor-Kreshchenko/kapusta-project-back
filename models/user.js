51dce3cda55cef0f68a6cb46e971c70324b17ac4
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-10-05 10:53:40 +0100

    moved files in controllers, mixed schema user

---
+++ models/user.js
@@ -0,0 +1,68 @@
+// const { Schema, model } = require("mongoose");
+// const Joi = require("joi");
+
+// const emailRegexp =
+//   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// const UserSchema = Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, "name is required"],
+//     },
+//     email: {
+//       type: String,
+//       match: emailRegexp,
+//       required: [true, "Email is required"],
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: [true, "Password is required"],
+//     },
+//     avatarUrl: {
+//       type: String,
+//       default: () => {
+//         return gravatar.url(this.email);
+//       },
+//     },
+//     verify: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     verifyToken: {
+//       type: String,
+//       required: [true, "Verify token is required"],
+//     },
+//     accessToken: {
+//       type: String,
+//       default: null,
+//     },
+//   },
+//   { versionKey: false, timestamps: true }
+// );
+
+// const joiSchemaAddUser = Joi.object({
+//   password: Joi.string().required(),
+//   email: Joi.string().email(emailRegexp).required(),
+//   name: Joi.string().required(),
+// });
+
+// const joiSchemaChangeUser = Joi.object({
+//   password: Joi.string(),
+//   email: Joi.string().email(emailRegexp),
+//   name: Joi.string(),
+// });
+
+// const joiSchemaVerifyEmail = Joi.object({
+//   email: Joi.string().email(emailRegexp).required(),
+// });
+
+// const User = model("user", UserSchema);
+
+// module.exports = {
+//   User,
+//   joiSchemaAddUser,
+//   joiSchemaChangeUser,
+//   joiSchemaVerifyEmail,
+// };
