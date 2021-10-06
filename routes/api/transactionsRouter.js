5b9bdd386e90b74ae2628e593619001bf9ab889f
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-10-06 22:27:14 +0300

    Update transactionsRouter.js

--- routes/api/transactionsRouter.js
+++ routes/api/transactionsRouter.js
@@ -1,16 +1,22 @@
-const express = require('express')
+const express = require("express");
 const ctrl = require("../../controllers/transactions");
 const transactionsRouter = express.Router();
-const { controllerWrapper } = require("../../middlewares")
+const { controllerWrapper } = require("../../middlewares");
 
-router.get('/:operationType', ctrl.getOperations);
+transactionsRouter.get("/:operationType", ctrl.getOperations);
 
-transactionsRouter.patch('/transactions/:operationType', controllerWrapper(ctrl.addOperations));
+transactionsRouter.patch(
+  "/transactions/:operationType",
+  controllerWrapper(ctrl.addOperations)
+);
 
 // transactionsRouter.patch('/expenses', controllerWrapper(ctrl.addExpense));
 
-transactionsRouter.patch('/balance', controllerWrapper(ctrl.renewBalance));
+transactionsRouter.patch("/balance", controllerWrapper(ctrl.renewBalance));
 
-transactionsRouter.patch('/:operationType/:operationId', controllerWrapper(ctrl.deleteOperationFromTransactions));
+transactionsRouter.patch(
+  "/:operationType/:operationId",
+  controllerWrapper(ctrl.deleteOperationFromTransactions)
+);
 
 module.exports = transactionsRouter;
