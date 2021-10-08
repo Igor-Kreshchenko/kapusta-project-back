3be16051e3b00b9a002d286e126a84a2e307e5a1
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-10-08 16:19:17 +0100

    changed architecture

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,14 @@
-const mongoose = require("mongoose");
-// const db = require("../models/db");
+const db = require("../db/db");
 require("dotenv").config();
 
 const app = require("../app");
 
 const { DB_HOST, PORT = 4000 } = process.env;
 
-// db.then(
-//   app.listen(PORT, () => {
-//     console.log(`Server running. Use our API on port: ${PORT}`);
-//   })
-// ).catch((error) => console.log(error.message));
-
-mongoose
-  .connect(DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+db.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     app.listen(PORT);
     console.log(`Server running. Use our API on port: ${PORT}`);
