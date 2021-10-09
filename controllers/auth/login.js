534d510ff66393f947868d3cec888a78a2fd1c34
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-10-09 12:57:35 +0300

    Добавляет проверку на пароль при логинизации

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,15 +1,18 @@
 const { findByEmail, updateToken } = require("../../repositories/users");
+const bcrypt = require("bcryptjs");
 const HttpCode = require("../../helpers/constants");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
-const login = async (req, res, next) => {
-  const { email } = req.body;
+const login = async (req, res) => {
+  const { email, password } = req.body;
   const user = await findByEmail(email);
-  //   const isValidPassword = await user?.validPassword(password);
-  //   if (!user || !isValidPassword) {
-  if (!user) {
+
+  const hashPassword = user.password;
+  const compareResult = bcrypt.compareSync(password, hashPassword);
+
+  if (!user || !compareResult) {
     return res.status(HttpCode.UNAUTHORIZED).json({
       status: "error",
       code: HttpCode.UNAUTHORIZED,
@@ -18,11 +21,13 @@ const login = async (req, res, next) => {
       },
     });
   }
+
   const payload = { id: user.id };
   const token = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: "2h" });
   await updateToken(user.id, token);
+
   return res.status(HttpCode.OK).json({
-    status: "Ok",
+    status: "OK",
     contentType: "application/json",
     code: HttpCode.OK,
     responseBody: {
