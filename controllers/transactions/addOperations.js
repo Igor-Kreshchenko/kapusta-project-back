f458eed50463ad65351e015d96a2c64bb43a5e2b
Oleksii Patsiuk <patsyuck@ukr.net>
2021-10-10 23:32:37 +0300

    Описав ендпойнт додавання транзакції по витратах/доходу

--- controllers/transactions/addOperations.js
+++ controllers/transactions/addOperations.js
@@ -1,8 +1,14 @@
 const { findTransactions, addOperation } = require("../../utils");
+const HttpCode = require("../../helpers/constants");
 
 const addOperations = async (req, res, _) => {
   const { operationType } = req.params;
   const { body } = req;
+  if ((!body.amount) || (!body.date) || (!body.category) || (!body.description)) {
+    return res.status(HttpCode.BAD_REQUEST).json({
+      message: "'amount', 'date', 'category', and 'description' are required fields"
+    });
+  };
   const { _id: userId } = req.user;
 
   const userTransactions = await findTransactions(userId);
