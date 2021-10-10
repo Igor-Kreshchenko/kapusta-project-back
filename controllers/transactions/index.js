3f35a404c0b8f808cecbdd9e4f8508f588907e8e
Oleksii Patsiuk <patsyuck@ukr.net>
2021-10-11 00:16:16 +0300

    Описав ендпойнт видалення транзакції

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -1,11 +1,13 @@
 const addOperations = require("./addOperations");
 const renewBalance = require("./renewBalance");
-const deleteOperationFromTransactions = require("./deleteOperationFromTransactions")
+const deleteOperationFromTransactions = require("./deleteOperationFromTransactions");
+//const deleteTransaction = require("./deleteTransaction");
 const getOperations = require("./getOperations");
 
 module.exports = {
     addOperations,
     renewBalance,
     deleteOperationFromTransactions,
+    //deleteTransaction,
     getOperations,
 }
