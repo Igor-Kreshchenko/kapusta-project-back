14ef4e93d2628607cca6871bf9640943c6f9cbcf
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-10-11 08:24:14 +0100

    balance with authentication

--- controllers/transactions/getBalance.js
+++ controllers/transactions/getBalance.js
@@ -1,22 +1,14 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET_KEY } = process.env;
-// const { findTransactions } = require("../../utils");
+const { findTransactions } = require("../../utils");
 const HttpCode = require("../../helpers/constants");
-const { Transaction } = require("../../models");
-// const transactions = require("../../repositories/transactions");
 
-const getBalance = async (req, res, next) => {
-  try {
-    const { id } = jwt.verify(token, JWT_SECRET_KEY);
-    const { balance } = await Transaction.findOne({ _id: id }, balance);
-    console.log(balance);
-    return res.status(HttpCode.CREATED).json({
-      status: "success",
-      code: HttpCode.CREATED,
-      balance,
-    });
-  } catch (error) {
-    next(error);
-  }
+const getBalance = async (req, res) => {
+  const { _id: userId } = req.user;
+  const { balance } = await findTransactions(userId);
+
+  return res.status(HttpCode.CREATED).json({
+    status: "success",
+    code: HttpCode.CREATED,
+    balance,
+  });
 };
 module.exports = getBalance;
--- routes/api/transactionsRouter.js
+++ routes/api/transactionsRouter.js
@@ -3,6 +3,18 @@ const ctrl = require("../../controllers/transactions");
 const transactionsRouter = express.Router();
 const { controllerWrapper, authenticate } = require("../../middlewares");
 
+transactionsRouter.get(
+  "/balance",
+  controllerWrapper(authenticate),
+  controllerWrapper(ctrl.getBalance)
+);
+
+transactionsRouter.patch(
+  "/balance",
+  controllerWrapper(authenticate),
+  controllerWrapper(ctrl.renewBalance)
+);
+
 transactionsRouter.get("/:operationType", ctrl.getOperations);
 
 transactionsRouter.patch(
@@ -16,13 +28,5 @@ transactionsRouter.delete(
 );
 
 // transactionsRouter.patch('/expenses', controllerWrapper(ctrl.addExpense));
-
-transactionsRouter.get(
-  "/balance",
-  controllerWrapper(authenticate),
-  controllerWrapper(ctrl.getBalance)
-);
-
-transactionsRouter.patch("/balance", controllerWrapper(ctrl.renewBalance));
 
 module.exports = transactionsRouter;
